9c420bed0fe4d38624df3589cbeb358bb770bc6c
Zane Gray <zanejgr@gmail.com>
2019-01-26 22:20:08 +0000

    added makefile,updated size of diamond to spec, added ability to choose character

--- diamond.c
+++ diamond.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc,char*argv[]){
-	for(int i = 0; i < atoi(argv[1]);i++){
-		for(int j = 0; j < atoi(argv[1])-i;j++){
+	char c = *argv[2];
+	for(int i = 0; i < atoi(argv[1])-1;i++){
+		for(int j = 0; j < atoi(argv[1])-1-i;j++){
 			putchar(' ');
 		}
 		for(int j = 0; j < i;j++){
-			putchar('*');
+			putchar(c);
 		}
-		putchar('*');
+		putchar(c);
 		for(int j = 0; j < i;j++){
-			putchar('*');
+			putchar(c);
 		}
 		putchar('\n');
 	}
-	for(int i = atoi(argv[1]);i>=0;i--){
+	for(int i = atoi(argv[1]);i>0;i--){
 		for(int j = 0; j < atoi(argv[1])-i;j++){
 			putchar(' ');
-		}for(int j = 0; j < i; j++){
-			putchar('*');
+		}for(int j = 0; j < i-1; j++){
+			putchar(c);
 		}
-		putchar('*');
-		for(int j = 0; j < i; j++){
-			putchar('*');
+		putchar(c);
+		for(int j = 0; j < i-1; j++){
+			putchar(c);
 		}
 		putchar('\n');
 	}
---
+++ makefile
@@ -0,0 +1,4 @@
+all:
+	gcc -g -O3 diamond.c -o diamond
+clean:
+	rm  diamond
